e8ec96bcb072228a8aeb83553dc1ae97a2a407c8
Ruan <ruan4f@gmail.com>
2019-02-05 21:42:51 -0200

    Create a controller tweet

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,11 +10,8 @@ mongoose.connect(
   }
 );
 
-
-
-app.get('/', (req, res) => {
-  return res.send('Hello World');
-});
+app.use(express.json());
+app.use(require('./routes'));
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const routes = express.Router();
 
+const TweetController = require('./controllers/TweetController');
+
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store);
+
 module.exports = routes;
