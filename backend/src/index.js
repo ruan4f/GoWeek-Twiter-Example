d702f2173af9abd2958895d2831f5d85db7f0215
Ruan <ruan4f@gmail.com>
2019-02-05 21:54:01 -0200

    Include socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,21 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(
   'mongodb://ruan:rf123456@ds123465.mlab.com:23465/goweek-ruan',
   {
     useNewUrlParser: true
   }
 );
+
+app.use((req, res, next) => {
+  req.io = io;
+  
+  return next();
+})
 
 app.use(express.json());
 app.use(require('./routes'));
