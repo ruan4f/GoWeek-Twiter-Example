52159c7b7e3ee5697d885449fa5056a9970de4e8
Ruan <ruan4f@gmail.com>
2019-02-05 21:26:06 -0200

    File initial to route api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    'mongodb://ruan:rf123456@ds123465.mlab.com:23465/goweek-ruan', 
-    {
-        useNewUrlParser: true
-    }
-);
-
 const app = express();
 
+mongoose.connect(
+  'mongodb://ruan:rf123456@ds123465.mlab.com:23465/goweek-ruan',
+  {
+    useNewUrlParser: true
+  }
+);
+
+
+
 app.get('/', (req, res) => {
-    return res.send('Hello World');
+  return res.send('Hello World');
 });
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  console.log('Server started on port 3000');
 });
