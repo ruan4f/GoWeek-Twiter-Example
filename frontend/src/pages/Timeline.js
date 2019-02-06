3ceb07c8d8395920d22fa46d8e90903f0f591a05
Ruan Ferreira <ruan4f@gmail.com>
2019-02-06 14:20:15 -0200

    Add timeline page

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 
 // import { Container } from './styles';
 
+import twitterLogo from '../twitter.svg';
+import './Timeline.css';
+
 export default class Timeline extends Component {
+    state = {
+        newTweet: '',
+    };
+
+    handleNewTeet = e => {
+        if (e.keyCode !== 13) return;
+
+        const content = this.state.newTweet;
+        const author = localStorage.getItem('@GoTwitter:username');
+    }
+
+    handleInputChange = e => {
+        this.setState({ newTweet: e.target.value });
+    }
+
     render() {
-        return (<h1>Timeline</h1>);
+        return (
+            <div className="timeline-wrapper">
+                <img height={24} src={twitterLogo} alt="Go Twitter" />
+
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTeet}
+                        placeholder="O que está acontecendo?" />
+                </form>
+
+                <ul>
+
+                </ul>
+            </div>
+        );
     }
 }
