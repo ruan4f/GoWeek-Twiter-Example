5312811c8ec26ae94a56863f7b20b3f32bd6202a
Ruan Ferreira <ruan4f@gmail.com>
2019-02-06 14:00:22 -0200

    Include new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Login from './pages/Login';
+import Timeline from './pages/Timeline';
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={}/>
+          <Route path="/" exact component={Login} />
+          <Route path="/timeline" component={Timeline} />
         </Switch>
-      </BrowserRouter>      
+      </BrowserRouter>
     );
   }
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <h1>Login</h1>
+        );
+    }
+}
