b41c7e5fe8b8e5dd4e68752000bf3032321465ec
Ruan <ruan4f@gmail.com>
2019-02-07 20:35:05 -0200

    Base structure app

---
+++ omnistackrn/src/pages/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 30
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    height: 44,
+    paddingHorizontal: 15,
+    alignSelf: "stretch",
+    marginTop: 30
+  },
+
+  button: {
+    height: 44,
+    alignSelf: "stretch",
+    marginTop: 10,
+    backgroundColor: "#4BB0EE",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  buttonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold"
+  }
+});
+
---
+++ omnistackrn/src/routes.js
@@ -0,0 +1,12 @@
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+
+import Login from './pages/Login';
+
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    Login,
+
+  })
+);
+
+export default Routes;
